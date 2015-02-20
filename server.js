b0ff1f88aba31d24391bad3ac8d35650aaa646ae
Ionut Costica <ionut.costica@gmail.com>
2015-02-20 10:25:27 +0200

    Begin node.js webserver

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+router.use(function (req, res, next) {
+	console.log('%s %s %s', req.method, req.url, req.path);
+	next();
+});
+router.route('/').all(function (req, res, next) {
+	console.log('redirecting /');
+	req.url = '/index.html';
+	next();
+});
+router.use(express.static(__dirname + '/app'));
+app.use('/', router);
+
+var server = app.listen(8888, function () {
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log('Example app listening at http://%s:%s', host, port)
+});
